b6446fba4c6e5f8742327995b224920b4b2e24a9
aaron <zhw.china@gmail.com>
2019-06-13 17:36:06 +0800

    .

--- deamon.php
+++ deamon.php
@@ -7,7 +7,8 @@
      */
     try{
         $isDev = '';
-        require_once 'TaskQueueConfig.php';
+        
+		require_once 'TaskQueueConfig.php';
         $taskHost = !isset( $argv[1] ) ? DEFAULT_QUEUE_HOST : $argv[1];
         $isDev = isset( $argv[2] ) && $argv[2] == 'dev' ?  'dev' : '';
         $TaskQueueDeamon = new TaskQueueDeamon( $taskHost );
